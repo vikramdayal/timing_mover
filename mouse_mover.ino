b82353448593b53207ca26b65f0e2382a345a646
Vikram Dayal <vdayal@avman.net>
2021-12-28 09:02:20 -0800

    Source code

---
+++ mouse_mover.ino
@@ -0,0 +1,28 @@
+/* Mouse mover: adapted from the following example to control servo motors.
+Servo motor with Arduino example code. 
+Position and sweep. More info: https://www.makerguides.com/ */
+
+// Include the servo library:
+#include <Servo.h>
+
+// Create a new servo object:
+Servo myservo;
+
+// Define the servo pin:
+#define servoPin 9
+
+// Create a variable to store the servo position:
+int angle = 0;
+
+void setup() {
+  // Attach the Servo variable to a pin:
+  myservo.attach(servoPin);
+}
+
+void loop() {
+  // Tell the servo to go to a particular angle:
+  myservo.write(10);
+  delay(1000);
+  myservo.write(0);
+  delay(1000);
+}
